75213f6437e0bb86cd7d6bd54958cf8f548de544
Satyam Shukla <Sat02yam@gmail.com>
2021-08-16 23:45:08 +0530

    All Files added

---
+++ Category/Javascript/app.js
@@ -0,0 +1,109 @@
+function socialWelfare() {
+    var x = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var b = document.getElementById("sp");
+    var c = document.getElementById("re");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+function education() {
+    var x = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var b = document.getElementById("sp");
+    var c = document.getElementById("re");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    y.style.display = "none";
+  }
+}
+function food() {
+    var x = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var b = document.getElementById("sp");
+    var c = document.getElementById("re");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+    y.style.display = "none";
+    x.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    z.style.display = "none";
+  }
+}
+function women() {
+    var x = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var b = document.getElementById("sp");
+    var c = document.getElementById("re");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    x.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    a.style.display = "none";
+  }
+}
+function sports() {
+    var b = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var x = document.getElementById("sp");
+    var c = document.getElementById("re");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+function religion() {
+    var c = document.getElementById("sw");
+    var y = document.getElementById("ed");
+    var z = document.getElementById("fd");
+    var a = document.getElementById("wm");
+    var b = document.getElementById("sp");
+    var x = document.getElementById("re");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
