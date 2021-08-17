6a43b68e986d0ecc43fa96cb6319c51f5a3fdc22
Satyam Shukla <Sat02yam@gmail.com>
2021-08-17 21:55:45 +0530

    Organized everything

---
+++ Universal Files/Card/ngo.js
@@ -0,0 +1,331 @@
+
+ var obj = [
+    {
+        "Name of Organization ": "Immaculate Heart of Mary Social Service Society",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Puducherry",
+        "Thematic Area": "Rural Development",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Indian Rugby Football Union",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Maharashtra",
+        "Thematic Area": "Health",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "HelpAge India",
+        "Category": "B",
+        "Donor Partner": "Others",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Huda educational and Social Welfare Society",
+        "Category": "B",
+        "Donor Partner": "Government",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Health",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Humana People to People India (HPPI)",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Women empowerment",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Karnataka",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Kerala",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Lakshwadeep",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Puducherry",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Tamil Nadu",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Research and Advocacy",
+        "Operations State": "Telangana",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "Government",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Puducherry",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Pravara Medical Trust ",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Maharashtra",
+        "Thematic Area": "Rural Development",
+        "Operations State": "Maharashtra",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Progressive Life Center",
+        "Category": "B",
+        "Donor Partner": "Others",
+        "State of Registration": "Maharashtra",
+        "Thematic Area": "Education",
+        "Operations State": "Maharashtra",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "No"
+    },
+    {
+        "Name of Organization ": "Preservation and Proliferation of Rural Resources and Nature(PRAN)",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Bihar",
+        "Thematic Area": "Rural Development",
+        "Operations State": "Odisha",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JOINT WOMEN'S PROGRAMME",
+        "Category": "B",
+        "Donor Partner": "Government",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Delhi",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "PIPAL TREE FOUNDATION",
+        "Category": "B",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Maharashtra",
+        "Thematic Area": "Women empowerment",
+        "Operations State": "Maharashtra",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Paschim Sridharkati Janakalyan Sangha",
+        "Category": "B",
+        "Donor Partner": "International Agency",
+        "State of Registration": "West Bengal",
+        "Thematic Area": "Environment",
+        "Operations State": "West Bengal",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "parinaama development foundation",
+        "Category": "B",
+        "Donor Partner": "Government",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Odisha",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "ONKAR SEVA SANSTHAN",
+        "Category": "B",
+        "Donor Partner": "Government",
+        "State of Registration": "Jharkhand",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Jharkhand",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "NAV BIHAR SAMAJ KALYAN PRATISTHAN KENDRA",
+        "Category": "A",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Bihar",
+        "Thematic Area": "Environment",
+        "Operations State": "Bihar",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "National Skills Foundation of India",
+        "Category": "B",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Haryana",
+        "Thematic Area": "Women empowerment",
+        "Operations State": "Jharkhand",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Mother Care Child Development Society",
+        "Category": "B",
+        "Donor Partner": "Others",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Environment",
+        "Operations State": "Delhi",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "No"
+    },
+    {
+        "Name of Organization ": "Lok Swasthya SEWA Trust",
+        "Category": "B",
+        "Donor Partner": "International Agency",
+        "State of Registration": "Gujarat",
+        "Thematic Area": "Rural Development",
+        "Operations State": "Delhi",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Karm Marg Charitable Society",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Women empowerment",
+        "Operations State": "Delhi",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "No"
+    },
+    {
+        "Name of Organization ": "JOINT WOMEN'S PROGRAMME",
+        "Category": "B",
+        "Donor Partner": "Government",
+        "State of Registration": "Delhi",
+        "Thematic Area": "Education",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "Government",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Tamil Nadu",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "Centre for Socio-economic and environmental studies",
+        "Category": "A",
+        "Donor Partner": "Government",
+        "State of Registration": "Kerala",
+        "Thematic Area": "Livelihood",
+        "Operations State": "Telangana",
+        "Worked with any Japanese Organization ": "Yes",
+        "Worked with any State or National Government ": "Yes"
+    }
+]
+
+
+  var myData= document.getElementsByClassName("card");
+    var ctitle= document.getElementsByClassName("card_title");
+    var cbody= document.getElementsByClassName("card_body");
+    var sel = document.getElementById("dropdown");
+  
+   
+    sel.onclick= function(){
+        console.log(sel.value);
+        for(let i=0;i<myData.length;i++)
+        {
+        if(obj[i]["Operations State"]===sel.value)
+       { myData[i].style.display="block"
+           myData[i].classList.add("card");
+        cbody[i].innerText= obj[i]["Operations State"];
+        ctitle[i].innerText=obj[i]["Name of Organization "]
+        
+        let li = document.createElement('p');
+        li.textContent = obj[i].Category;
+        myData[i].appendChild(li);}
+        else{
+            myData[i].style.display="none";
+        }
+            
+    
+        //    let tag = document.createElement("p");
+        //    let text = document.createTextNode(obj[i].Category);
+        //    tag.appendChild(text);
+        //    myData.appendChild(tag);
+    
+             //create new li element
+             //add it to the ul element
+        
+    }
+    }
+
+
