d1d4eaa4fb58e24acaa9a747789e0f121ec2d506
Rucha <rucha.kasture2002@gmail.com>
2021-09-01 16:20:39 +0530

    added cards with js

--- final.js
+++ final.js
@@ -1,5 +1,6 @@
 var obj = [
     {
+        "id":"1",
         "Name of Organization ": "Immaculate Heart",
         "Category": "A",
         "Donor Partner": "Corporate",
@@ -7,9 +8,11 @@ var obj = [
         "Thematic Area": "Rural Development",
         "Operations State": "Uttar Pradesh",
         "Worked with any Japanese Organization ": "No",
-        "Worked with any State or National Government ": "Yes"
+        "Worked with any State or National Government ": "Yes",
+        "link" : "https://www.geeksforgeeks.org"
     },
     {
+        "id":"2",
         "Name of Organization ": "IndianRugbyFootball Union",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -17,9 +20,11 @@ var obj = [
         "Thematic Area": "Health",
         "Operations State": "Uttar Pradesh",
         "Worked with any Japanese Organization ": "Yes",
-        "Worked with any State or National Government ": "Yes"
+        "Worked with any State or National Government ": "Yes",
+        "link" : "https://www.geeksforgeeks.org"
     },
     {
+        "id":"3",
         "Name of Organization ": "HelpAge India",
         "Category": "B",
         "Donor Partner": "Others",
@@ -30,6 +35,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"4",
         "Name of Organization ": "Huda ESWS",
         "Category": "B",
         "Donor Partner": "Government",
@@ -40,6 +46,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"5",
         "Name of Organization ": "Humana People",
         "Category": "A",
         "Donor Partner": "Corporate",
@@ -50,6 +57,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"6",
         "Name of Organization ": "Centre for SE and ES",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -60,6 +68,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"7",
         "Name of Organization ": "Centre for SE and ES",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -70,6 +79,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"8",
         "Name of Organization ": "Centre for SE and ES",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -80,6 +90,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"9",
         "Name of Organization ": "Centre for SE and ES",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -90,6 +101,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"10",
         "Name of Organization ": "Centre for SE and ES",
         "Category": "A",
         "Donor Partner": "Government",
@@ -100,6 +112,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"11",
         "Name of Organization ": "Pravara Medical ",
         "Category": "A",
         "Donor Partner": "Corporate",
@@ -110,6 +123,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"12",
         "Name of Organization ": "Progressive Life",
         "Category": "B",
         "Donor Partner": "Others",
@@ -120,6 +134,7 @@ var obj = [
         "Worked with any State or National Government ": "No"
     },
     {
+        "id":"13",
         "Name of Organization ": "PRAN",
         "Category": "A",
         "Donor Partner": "Corporate",
@@ -130,6 +145,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"14",
         "Name of Organization ": "Joint Women's Prog.",
         "Category": "B",
         "Donor Partner": "Government",
@@ -140,6 +156,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"15",
         "Name of Organization ": "Pipal Tree Fdn.",
         "Category": "B",
         "Donor Partner": "Corporate",
@@ -150,6 +167,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"16",
         "Name of Organization ": "Paschim Sridharkati",
         "Category": "B",
         "Donor Partner": "International Agency",
@@ -160,6 +178,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"17",
         "Name of Organization ": "parinaama Dev Fdn.",
         "Category": "B",
         "Donor Partner": "Government",
@@ -170,6 +189,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"18",
         "Name of Organization ": "Onkar Seva Sansthan",
         "Category": "B",
         "Donor Partner": "Government",
@@ -180,6 +200,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"19",
         "Name of Organization ": "NAV BIHAR SAMAJ KALYAN PRATISTHAN KENDRA",
         "Category": "A",
         "Donor Partner": "International Agency",
@@ -190,6 +211,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"20",
         "Name of Organization ": "National Skills Fdn.",
         "Category": "B",
         "Donor Partner": "Corporate",
@@ -200,6 +222,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"21",
         "Name of Organization ": "MotherCareChild DS",
         "Category": "B",
         "Donor Partner": "Others",
@@ -210,6 +233,7 @@ var obj = [
         "Worked with any State or National Government ": "No"
     },
     {
+        "id":"22",
         "Name of Organization ": "Lok Swasthya SEWA",
         "Category": "B",
         "Donor Partner": "International Agency",
@@ -220,6 +244,7 @@ var obj = [
         "Worked with any State or National Government ": "Yes"
     },
     {
+        "id":"23",
         "Name of Organization ": "KarmMarg Charitable Society",
         "Category": "A",
         "Donor Partner": "Corporate",
@@ -230,11 +255,34 @@ var obj = [
         "Worked with any State or National Government ": "No"
     },
     {
+        "id":"24",
         "Name of Organization ": "Joint Women's Prgm.",
         "Category": "B",
         "Donor Partner": "Government",
         "State of Registration": "Delhi",
         "Thematic Area": "Education",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "id":"25",
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "id":"26",
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
         "Operations State": "Uttar Pradesh",
         "Worked with any Japanese Organization ": "No",
         "Worked with any State or National Government ": "Yes"
@@ -249,12 +297,86 @@ var obj = [
         "Worked with any Japanese Organization ": "No",
         "Worked with any State or National Government ": "Yes"
     },
-
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    },
+    {
+        "Name of Organization ": "JAN KALYAN MAHA SAMITI",
+        "Category": "A",
+        "Donor Partner": "Corporate",
+        "State of Registration": "Uttar Pradesh",
+        "Thematic Area": "Agriculture",
+        "Operations State": "Uttar Pradesh",
+        "Worked with any Japanese Organization ": "No",
+        "Worked with any State or National Government ": "Yes"
+    }
+    
 ]
 var myData= document.getElementsByClassName("card");
 var ctitle= document.getElementsByClassName("card_title");
 var cbody= document.getElementsByClassName("card_body");
+var addCard = document.getElementById("list");
+var mul_card = document.getElementsByClassName("multiple_cards");
 
+    var sel = document.getElementById("dropdown");
+  
 for(let i=0;i<myData.length;i++)
 {
     // var sel = document.getElementById("dropdown");
@@ -276,34 +398,86 @@ for(let i=0;i<myData.length;i++)
     //     myData[i].style.display="none";
     // }
 }
-var myData= document.getElementsByClassName("card");
-    var ctitle= document.getElementsByClassName("card_title");
-    var cbody= document.getElementsByClassName("card_body");
-    var sel = document.getElementById("dropdown");
-  
-   
+    
     sel.onclick= function(){
         console.log(sel.value);
         for(let i=0;i<myData.length;i++)
         {
-        if(obj[i]["Operations State"]===sel.value)
-       { myData[i].style.display="block"
+            if(sel.value==="All")
+            {
+                myData[i].style.display="block"
            myData[i].classList.add("card");
+           cbody[i].innerText= obj[i]["Operations State"];
+           ctitle[i].innerText=obj[i]["Name of Organization "]
+        }
+        else if(obj[i]["Operations State"]===sel.value)
+        { myData[i].style.display="block"
+        myData[i].classList.add("card");
         cbody[i].innerText= obj[i]["Operations State"];
         ctitle[i].innerText=obj[i]["Name of Organization "]
-        }
-        else{
-            myData[i].style.display="none";
-        }
-            
-    
-        //    let tag = document.createElement("p");
-        //    let text = document.createTextNode(obj[i].Category);
-        //    tag.appendChild(text);
-        //    myData.appendChild(tag);
-    
-             //create new li element
-             //add it to the ul element
-        
     }
+    else{
+        myData[i].style.display="none";
     }
+    
+    
+    //    let tag = document.createElement("p");
+    //    let text = document.createTextNode(obj[i].Category);
+    //    tag.appendChild(text);
+    //    myData.appendChild(tag);
+    
+    //create new li element
+    //add it to the ul element
+    
+}
+}
+
+for(let i=0;i<30;i++)
+{
+    // document.createElement(mul_card[i]);
+    // mul_card[i].style.display="block";
+    // // mul_card.style.margin="20px";
+    // mul_card[i].style.padding="20px";
+    // // mul_card.style.height="200px";
+    // // mul_card.stykle.position="absolute";[i]
+    // mul_card[i].style.position="relative";
+    // // mul_card.style.zIndex="10";
+    // mul_card[i].style.background="blue";
+    
+    // mul_card[i].classList.add("card");
+    // cbody.innerText= obj[i]["Operations State"];
+    // ctitle.innerText=obj[i]["Name of Organization "]
+    // addCard.appendChild(mul_card[i]);
+
+    var tag = document.createElement("div");
+    var tagIn = document.createElement("div");
+    var head = document.createElement("h1");
+    var textHead = document.createTextNode(obj[i]["Name of Organization "]);
+    head.appendChild(textHead);
+    head.classList.add("card_title");
+    var para = document.createElement("p");
+    var textPara = document.createTextNode(obj[i]["Operations State"]);
+    para.appendChild(textPara);
+    para.classList.add("card_body");
+    // var link = document.createElement("a");
+    // link.href="/Category/blank.html";
+    // link.title="Explore";
+    var a = document.createElement('a'); 
+    var link = document.createTextNode("Explore");
+    a.appendChild(link); 
+    // a.title = "This is Link"; 
+    a.href = obj[i].link; 
+    a.target="_blank";
+    a.classList.add("button");
+   tagIn.appendChild(head);
+   tagIn.appendChild(para);
+   tagIn.appendChild(a);
+   tagIn.classList.add("card_content");
+   tag.appendChild(tagIn);
+   tag.classList.add("card")
+   addCard.appendChild(tag);
+   addCard.style.flexWrap="wrap";
+}
+var fragment = create('<div class="someclass"><a href="www.example.com"><p>some text</p></a></div>'); 
+
+document.body.insertBefore(fragment, document.body.childNodes[0]);
